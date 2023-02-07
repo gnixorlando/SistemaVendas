416a0ea210541eb22c7316e4b0c95d5c15989339
gnixorlando <gabyorlandonascimento@gmail.com>
2023-02-06 21:10:31 -0300

    configuration

---
+++ src/main/java/io/github/gnixorlando/VendasApplication.java
@@ -0,0 +1,29 @@
+package io.github.gnixorlando;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld (){
+
+        return applicationName;
+    }
+
+    public static void main(String[] args) {
+        System.setProperty("server.port", "8093");
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
