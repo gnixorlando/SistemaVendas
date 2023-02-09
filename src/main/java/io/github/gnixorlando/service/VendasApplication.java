b4c374f6e0af9b35d07e852ff89b6154a5f4d347
gnixorlando <gabyorlandonascimento@gmail.com>
2023-02-08 22:26:52 -0300

    edit

--- src/main/java/io/github/gnixorlando/service/VendasApplication.java
+++ src/main/java/io/github/gnixorlando/service/VendasApplication.java
@@ -1,29 +1,13 @@
 package io.github.gnixorlando.service;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class VendasApplication {
 
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloWorld (){
-
-        return applicationName;
-    }
-
     public static void main(String[] args) {
-        System.setProperty("server.port", "8093");
         SpringApplication.run(VendasApplication.class, args);
     }
 }
