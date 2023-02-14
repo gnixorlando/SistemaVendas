9498e9c68f8376eeb49ae6d6eedf02cd5a82ac96
gnixorlando <gabyorlandonascimento@gmail.com>
2023-02-14 19:57:22 -0300

    refactor

--- src/main/java/io/github/gnixorlando/VendasApplication.java
+++ src/main/java/io/github/gnixorlando/VendasApplication.java
@@ -20,32 +20,32 @@ public class VendasApplication {
     public CommandLineRunner init(@Autowired Clientes clientes) {
         return args -> {
             System.out.println("Salvando clientes");
-            clientes.salvar(new Cliente("Matheus"));
-            clientes.salvar(new Cliente("Junior"));
+            clientes.save(new Cliente("Matheus"));
+            clientes.save(new Cliente("Junior"));
 
 
-            List<Cliente> todosClientes = clientes.obterTodos();
+            List<Cliente> todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Atualizando clientes");
             todosClientes.forEach(c -> {
                 c.setNome(c.getNome() + " atualizado!");
-                clientes.atualizar(c);
+                clientes.save(c);
 
             });
 
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             todosClientes.forEach(System.out::println);
 
             System.out.println("Buscando clientes");
-            clientes.buscarPorNome("M").forEach(System.out::println);
+            clientes.findByNomeLike("M").forEach(System.out::println);
 
             System.out.println("deletando clientes");
-            clientes.obterTodos().forEach(c -> {
-                clientes.deletar(c);
+            clientes.findAll().forEach(c -> {
+                clientes.delete(c);
            });
 
-            todosClientes = clientes.obterTodos();
+            todosClientes = clientes.findAll();
             if(todosClientes.isEmpty()){
                 System.out.println("Nenhum cliente encontrado.");
             }else{
--- src/main/java/io/github/gnixorlando/domain/repositorio/Clientes.java
+++ src/main/java/io/github/gnixorlando/domain/repositorio/Clientes.java
@@ -1,76 +1,15 @@
 package io.github.gnixorlando.domain.repositorio;
 
 import io.github.gnixorlando.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
+
 @Repository
-public class Clientes {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    EntityManager entityManager;
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
 
-    @Transactional
-    public Cliente salvar(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-        @Transactional
-    public Cliente atualizar(Cliente cliente) {
-
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if (!entityManager.contains(cliente)) {
-            cliente = entityManager.merge(cliente);
-
-        }
-        entityManager.remove(cliente);
-
-    }
-
-    @Transactional
-    public void deletar(Integer id) {
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-
-
-    }
-
-    @Transactional(readOnly = true)
-
-    public List<Cliente> buscarPorNome(String nome) {
-        String jpql = " select c from Cliente c where c.nome like :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> obterTodos() {
-        return entityManager
-                .createQuery("from Cliente", Cliente.class)
-                .getResultList();
-
-    }
-
-
+    List<Cliente> findByNomeLike(String nome);
 }
