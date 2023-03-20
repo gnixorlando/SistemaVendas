b3194cc5750a9e1c3bf090aaeff0db0ea3e2555d
gnixorlando <gabyorlandonascimento@gmail.com>
2023-03-20 20:01:07 -0300

    product map

--- src/main/java/io/github/gnixorlando/rest/controller/ProdutoController.java
+++ src/main/java/io/github/gnixorlando/rest/controller/ProdutoController.java
@@ -2,9 +2,13 @@ package io.github.gnixorlando.rest.controller;
 
 import io.github.gnixorlando.domain.entity.Produto;
 import io.github.gnixorlando.domain.repository.Produtos;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -26,4 +30,31 @@ public class ProdutoController {
         return produtos.save(produto);
 
     }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete (@PathVariable Integer id) {
+    produtos.findById(id).map(produto -> {
+        produtos.delete(produto);
+        return produto;
+
+    }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update (@PathVariable Integer id, @RequestBody Produto produto) {
+    produtos.findById(id).map(produtoExistente -> {
+            produto.setId(produtoExistente.getId());
+            produtos.save(produto);
+            return produtoExistente; }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Produto não encontrado"));
+    }
+
+    @GetMapping
+    public List<Produto> find (Produto filtro) {
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        return produtos.findAll(example);
+    }
+
 }
