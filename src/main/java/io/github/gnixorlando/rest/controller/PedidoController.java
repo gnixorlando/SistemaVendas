8e447c2f68a38c410fb4a840cc15ac0c9d552285
gnixorlando <gabyorlandonascimento@gmail.com>
2023-03-28 20:01:24 -0300

    commit

--- src/main/java/io/github/gnixorlando/rest/controller/PedidoController.java
+++ src/main/java/io/github/gnixorlando/rest/controller/PedidoController.java
@@ -1,9 +1,13 @@
 package io.github.gnixorlando.rest.controller;
 
 
+import io.github.gnixorlando.domain.entity.Pedido;
+import io.github.gnixorlando.rest.dto.PedidoDTO;
 import io.github.gnixorlando.service.PedidoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -14,4 +18,13 @@ public class PedidoController {
     public PedidoController(PedidoService service) {
         this.service = service;
     }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Integer save(@RequestBody PedidoDTO dto) {
+        Pedido pedido = service.salvar(dto);
+        return pedido.getId();
+
+    }
+
 }
--- src/main/java/io/github/gnixorlando/service/PedidoService.java
+++ src/main/java/io/github/gnixorlando/service/PedidoService.java
@@ -1,4 +1,9 @@
 package io.github.gnixorlando.service;
 
+import io.github.gnixorlando.domain.entity.Pedido;
+import io.github.gnixorlando.rest.dto.PedidoDTO;
+
 public interface PedidoService{
+
+    Pedido salvar (PedidoDTO dto);
 }
